79072acaa15b62613dbf68f375d03847d82c69b1
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2024-08-27 01:16:49 +0600

    init commit

---
+++ models/events.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string
+	Description string
+	Location    string
+	DateTime    time.Time
+	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	// this will help to save the event to the database later
+	events = append(events, e)
+}
